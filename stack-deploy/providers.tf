a59bdbde2f9157fb61a66ea080b9558fa4e6db02
Henry Martinez <7996045+martinezhenry@users.noreply.github.com>
2020-04-22 04:25:29 +0200

    deploy prd

--- stack-deploy/providers.tf
+++ stack-deploy/providers.tf
@@ -2,10 +2,12 @@
 provider "docker" {
   host = "tcp://${local.docker_server}:${local.docker_port}/"
 
+  #user = "hmartinez"
+  #password = "hmartinez"
   # -> or the following
   #ca_material = "${file(pathexpand("~/.docker/machine/machines/host-1/ca.pem"))}" # this can be omitted
-  cert_material = file(pathexpand("C:/Users/hmartinez/Documents/cert.pem"))
-  key_material = file(pathexpand("C:/Users/hmartinez/Documents/key.pem"))
+  #cert_material = file(pathexpand("C:/Users/Administrador.WIN-PQD035U15RD/Documents/cert.pem"))
+  #key_material = file(pathexpand("C:/Users/Administrador.WIN-PQD035U15RD/Documents/key.pem"))
 
   registry_auth {
     address = "processing-dev.novopayment.net:2202"
--- stack-deploy/variables-local.tf
+++ stack-deploy/variables-local.tf
@@ -1,8 +1,8 @@
 locals {
 
   datos = concat(var.export_list, ["docker stack deploy -c /home/hmartinez/terraform/docker-compose.yml $stack_name"])
-  docker_server = "172.20.3.30"
-  docker_port = 2376
+  docker_server = "172.17.37.93"
+  docker_port = 2000
   docker_user = "hmartinez"
   private_key_file = "C:/Users/hmartinez/Documents/ssh_keys/id_rsa.ppk"
   file_destination = "/home/hmartinez/terraform/docker-compose.yml"
